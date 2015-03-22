dac8181ac5a7c5287423450daba08f66174784ab
bakerstreet221bsn <bakerstreet221bsn@users.noreply.github.com>
2015-03-22 13:55:52 +0000

    .

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
+packages = ["tornado"]
 
 setup(
     name="PangeaDealer",
     version="1.0",
     description="Pangea Poker Dealer",
     author="bakerstreet221b",
+    install_requires=packages
 )
